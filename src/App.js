598e9a9471e552ee5156e5e9a22ed6e499eb18d6
CHSI\si1206 <samchen@ch-si.com.tw>
2021-09-02 17:26:39 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+function getButtonText(){
+  return '點我!';
+}
+
+const App = () =>{
+  const buttonText ='click me!';
+  const labelText = 'Enter Name :';
+  return(
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text"></input>
+      <button style={{ backgroundColor: 'blue', color: 'white'}}>
+        {getButtonText()}
+      </button>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -11,6 +11,20 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// const App = () =>{
+//   return(
+//     <div>
+//       <label class="label" for="name">
+//         Enter Name :
+//       </label>
+//       <input id="name" type="text"></input>
+//       <button style={{ backgroundColor: 'blue', color: 'white'}}>
+//         Submit
+//       </button>
+//     </div>
+//   );
+// };
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
